21854da0d716b1c3c8d88e2226b7cbcb57130b46
Emmanuel Nyandu Kagarabi <emmanuelnk@aims.ac.za>
2026-05-28 14:48:31 +0000

    Remove deprecated helper and import scripts

--- static/scripts/dpc/inspect_django_orm.py
+++
@@ -1,9 +0,0 @@
-from django.db import connection,reset_queries
-from dpcstruct.models import DpcStructMcsProperty
-reset_queries()
-mc = DpcStructMcsProperty.objects.get(mc_id='MC7')
-seqs = list(mc.sequences.all())
-for q in connection.queries:
-    print(f"Time: {q['time']}s")
-    print(f"SQL: {q['sql']}")
-    print()
--- static/scripts/dpc/zipper_vm.sh
+++
@@ -1,2 +0,0 @@
-cd /u/mdmc/enyanduk/internship_areasciencepark/vm_data || exit 1
-tar -czvf /u/mdmc/enyanduk/internship_areasciencepark/dpc_vm.tar.gz *
